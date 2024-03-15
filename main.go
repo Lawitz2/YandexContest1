0959ce3ec84042da9c1101a8f4a95ba3d0b90726
Lawitz2 <161970246+Lawitz2@users.noreply.github.com>
2024-03-15 19:34:38 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type flight struct {
+	m      int
+	status string
+}
+
+type flights []flight
+
+type rockets struct {
+	f   []flight
+	dur int
+}
+
+type list struct {
+	id  int
+	dur int
+}
+
+type lists []list
+
+func (a flights) Len() int           { return len(a) }
+func (a flights) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a flights) Less(i, j int) bool { return a[i].m < a[j].m }
+
+func (a lists) Len() int           { return len(a) }
+func (a lists) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a lists) Less(i, j int) bool { return a[i].id < a[j].id }
+
+func main() {
+	logs := make(map[int]rockets)
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Printf("error opening the file: %v", err)
+		return
+	}
+
+	defer file.Close()
+
+	fo, err := os.Create("output.txt")
+	if err != nil {
+		fmt.Printf("error creating the file: %v", err)
+		return
+	}
+
+	writer := bufio.NewWriter(fo)
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+
+	for scanner.Scan() {
+		dataslice := strings.Split(scanner.Text(), " ")
+		mins, _ := strconv.Atoi(dataslice[0])
+		mins2, _ := strconv.Atoi(dataslice[1])
+
+		mins = mins*60*24 + mins2*60 //go from day/hour/minute to minutes
+		mins2, _ = strconv.Atoi(dataslice[2])
+		mins += mins2
+
+		idx, _ := strconv.Atoi(dataslice[3])
+		r := logs[idx]
+
+		if r.f == nil {
+			r.f = make([]flight, 0, 128)
+		}
+
+		var fl flight
+		fl.status = dataslice[4]
+		fl.m = mins
+		r.f = append(r.f, fl)
+		logs[idx] = r
+	}
+
+	res := make(lists, 0, len(logs))
+
+	for i, r := range logs {
+		sort.Sort(flights(r.f))
+		delta := 0
+		for _, d := range r.f {
+			if d.status == "B" {
+				continue
+			}
+			if d.status == "A" {
+				delta = d.m
+			} else {
+				r.dur += d.m - delta
+			}
+		}
+		res = append(res, list{id: i, dur: r.dur})
+	}
+
+	sort.Sort(res)
+
+	for _, i := range res {
+		fmt.Fprintf(writer, "%d ", i.dur)
+	}
+
+	writer.Flush()
+}
